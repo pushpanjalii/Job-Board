64f2c597a704ed7dd2d6a05f3cba153dd726f94a
pushpanjali <vermapushpanjali5@gmail.com>
2024-12-07 17:38:49 +0530

    company controller changed

--- controllers/company.controller.js
+++ controllers/company.controller.js
@@ -80,8 +80,8 @@ export const updateCompany = async (req,res) => {
         const file =req.file;  //logo of company
 
         const updateData = {name, description, website, location};
-        const company = await Company.findByIdAndUpdate(req.params.id, updateData, {new: true});
-        if(!company) {
+        const company = await Company.findByIdAndUpdate(req.params.id, updateData, {new: true}); //here new returns the modified value of 
+        if(!company) {                                                                           //document rather then the original one.
             return res.status(404).json({
                 message:"Company not found",
                 success:false
